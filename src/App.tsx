feabcc8c53a45d45bf08cedbb0e6ce7e8090897a
Shreya Morishetty <morishetty.1@buckeyemail.osu.edu>
2025-12-15 09:47:09 -0500

    fixed builds

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Projects from "./pages/Projects";
-import Experience from "./pages/Experience";
-import Extracurriculars from "./pages/Extracurriculars";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
-import Fun from "./pages/Fun";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
+import Index from "@/pages/Index";
+import Projects from "@/pages/Projects";
+import Experience from "@/pages/Experience";
+import Extracurriculars from "@/pages/Extracurriculars";
+import Blog from "@/pages/Blog";
+import BlogPost from "@/pages/BlogPost";
+import Fun from "@/pages/Fun";
+import Contact from "@/pages/Contact";
+import NotFound from "@/pages/NotFound";
 
 const App = () => (
   <TooltipProvider>
